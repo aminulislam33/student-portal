00d3a8761761ca84e7d359e4a38af162ea5d3925
Aminul Islam <aminulislam03905@gmail.com>
2024-12-14 18:39:49 +0530

    [UPDATE]: added faculty field in Department.js

--- models/Department.js
+++ models/Department.js
@@ -13,6 +13,10 @@ const departmentSchema = new mongoose.Schema({
     description: {
         type: String
     },
+    faculty: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Teacher",
+    },
     head: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Teacher",
